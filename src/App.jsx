1d6deeb552805ea8131334ee3c762528b9e82561
Erick Guarionex <erickguarionexdev@gmail.com>
2025-01-05 02:50:06 -0400

    Agregando scroll reveal

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,16 @@ import MainImage from "./Components/MainImage"
 import Menu from "./Components/Menu"
 import MainContent from "./Components/MainContent"
 import Footer from "./Components/Footer"
+import { useEffect } from "react"
+
+
 
 export default function App(){
+
+  useEffect(() =>{
+    ScrollReveal().reveal('.container')
+  }, [])
+  
   return(
     <>
       <div className="container bg-black h-screen">
--- src/Components/MainImage.jsx
+++ src/Components/MainImage.jsx
@@ -1,9 +1,10 @@
 export default function MainImage(){
 
     const imagen = "/ERICKGUARIONEX-CURRICULUM (2).svg";
+    ScrollReveal().reveal('.imagen-principal', {delay: 500});
 
     return(
-        <div className="h-100vh">
+        <div className="h-100vh imagen-principal">
               <img width={'700px'} className="absolute right-[0px] bottom-[150px] z-50" src={imagen} alt="Imagen principal" />
         </div>
     )
