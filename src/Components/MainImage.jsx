dd831a4e6e46869c66ebc1994af2d1e0e347713b
Erick Guarionex <erickguarionexdev@gmail.com>
2025-03-15 18:02:20 -0400

    Cambio pequeño en imagen

--- src/Components/MainImage.jsx
+++ src/Components/MainImage.jsx
@@ -5,7 +5,7 @@ export default function MainImage(){
 
     return(
         <div className="h-100vh imagen-principal">
-              <img width={'700px'} className="absolute right-[0px] bottom-[150px] z-50" src={imagen} alt="Imagen principal" />
+              <img width={'500px'} className="absolute right-[0px] bottom-[150px] z-50" src={imagen} alt="Imagen principal" />
         </div>
     )
 }
